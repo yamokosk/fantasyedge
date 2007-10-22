222da991277328fa3193f939e91497550459cc27
yamokosk <yamokosk@f0bbfac0-403c-0410-a557-73fa6a0c6ba8>
2007-10-22 23:20:19 +0000

    getting there

--- trunk/loadData.m
+++ trunk/loadData.m
@@ -1,46 +1,108 @@
-function [data,matchups] = loadData(week)
+function pdata = loadData(week, p_home, p_away)
 
-% Formats and filenames
-in(1).format = '';
-in(1).filename = ['qb_week_' num2str(week) '.data'];
-in(2).format = '%s %s %s %f %*f %f %*f %*f %f %*f %f %*f %*f %*f %*f %*f %f %*f %f';
-in(2).filename = ['rb_week_' num2str(week) '.data'];
-in(3).format = '';
-in(3).filename = ['wr_week_' num2str(week) '.data'];
-in(4).format = '';
-in(4).filename = ['te_week_' num2str(week) '.data'];
-in(5).format = '';
-in(5).filename = ['k_week_' num2str(week) '.data'];
-in(6).format = '';
-in(6).filename = ['def_passing_week_' num2str(week) '.data'];
-in(7).format = '%s %s %f %*f %f %*f %*f %f %*f %*f';
-in(7).filename = ['def_receiving_week_' num2str(week) '.data'];
-in(8).format = '%s %s %f %*f %f %*f %*f %*f %f %*f %*f';
-in(8).filename = ['def_rushing_week_' num2str(week) '.data'];
+cpassYds = 1/50;
+crushYds = 1/25;
+crecYds = 1/25;
+cpassTD = 6;
+crushTD = 6;
+crecTD = 6;
+cint = -2;
+cfum = -2;
 
-data = [];
-for n = 1:length(in)
-    fid = fopen( in(n).filename );
-    
-    if ( ~strcmp(in(n).format, '') )
-        data(n).rawdata = textscan(fid, in(n).format, 'multipleDelimsAsOne', 1, ...
-                                                     'headerLines', 1);
-    end
-    fclose(fid);
-end
-
-
-% Matchup data
-fid = fopen(['schedule_week_' num2str(week+1) '.data']);
-expr = '(?<away>\w+)\s+@\s+(?<home>\w+)';
-matchups = {};
-while 1
-    tline = fgetl(fid);
-    if ~ischar(tline) break; end
-    
-    [tokens, game] = regexp(tline, expr, 'tokens', 'names');
-    
-    if ~isempty(tokens)
-        matchups = [matchups; {game.home, game.away}];
+pdata.senarioProbabilities = zeros(length(p_home) * length(p_away), 1);
+k = 1;
+for m = 1:length(p_home)
+    for n = 1:length(p_away)
+        pdata.senarioProbabilities(k) = p_home(m) * p_away(n);
+        k = k + 1;
     end
 end
+ns = length(pdata.senarioProbabilities);
+
+[data, matchups] = loadDataFromFiles(week);
+
+nqb = data.qb.num;
+nrb = data.rb.num;
+nwr = data.wr.num;
+nte = data.te.num;
+nk = data.k.num;
+ndef = 32;
+
+np = nqb + nrb + nwr + nte + nk + ndef;
+perf = zeros(np, ns);
+
+PassYdsBar = mean(data.def_passing.PassYds);
+PassTDBar = mean(data.def_passing.PassTD);
+IntBar = mean(data.def_passing.Int);
+RushYdsBar = mean(data.def_rushing.RushYds);
+RushTDBar = mean(data.def_rushing.RushTD);
+RecYdsBar = mean(data.def_receiving.RecYds);
+RecTDBar = mean(data.def_receiving.RecTD);
+
+k = 1;
+for m = 1:length(p_home)
+    for n = 1:length(p_away)
+        ptr = 1;
+        % QB performance
+        perf(1:ptr+nqb,k) = C_PassYds * (data.qb.PassYds) .* (data.def_passing.PassYds / PassYdsBar) + ...
+                                          cpassTD * passTDij * passTDk / passTDBar + ...
+                                          crushYds * rushYdsij * rushYdsk / rushYdsBar + ...
+                                          crushTD * rushTDij * rushTDk / rushTDBar + ...
+                                          cint * intij * intk / intBar;
+        
+        % RB performance
+        ptr = ptr + nqb + 1;
+        perf(ptr:ptr+nrb,k) = crushYds * rushYdsij * rushYdsk / rushYdsBar + ...
+                                          crushTD * rushTDij * rushTDk / rushTDBar + ...
+                                          crecYds * recYdsij * recYdsk / recYdsBar + ...
+                                          crecTD * recTDij * recTDk / recTDBar + ...
+                                          cfum * fumBar;
+        
+        % WR performance
+        ptr = ptr + nrb + 1;
+        perf(ptr:ptr+nwr,k) = crushYds * rushYdsij * rushYdsk / rushYdsBar + ...
+                                          crushTD * rushTDij * rushTDk / rushTDBar + ...
+                                          crecYds * recYdsij * recYdsk / recYdsBar + ...
+                                          crecTD * recTDij * recTDk / recTDBar + ...
+                                          cfum * fumBar;
+       
+        % TE performance
+        ptr = ptr + nwr + 1;
+        perf(ptr:ptr+nte,k) = crushYds * rushYdsij * rushYdsk / rushYdsBar + ...
+                                          crushTD * rushTDij * rushTDk / rushTDBar + ...
+                                          crecYds * recYdsij * recYdsk / recYdsBar + ...
+                                          crecTD * recTDij * recTDk / recTDBar + ...
+                                          cfum * fumBar;
+        
+        % K performance
+        ptr = ptr + nte + 1;
+        perf(ptr:ptr+nk,k) = crushYds * rushYdsij * rushYdsk / rushYdsBar + ...
+                                          crushTD * rushTDij * rushTDk / rushTDBar + ...
+                                          crecYds * recYdsij * recYdsk / recYdsBar + ...
+                                          crecTD * recTDij * recTDk / recTDBar + ...
+                                          cfum * fumBar;
+        
+        % DEF performance
+        ptr = ptr + nk + 1;
+        perf(ptr:ptr+ndef,k) = crushYds * rushYdsij * rushYdsk / rushYdsBar + ...
+                                          crushTD * rushTDij * rushTDk / rushTDBar + ...
+                                          crecYds * recYdsij * recYdsk / recYdsBar + ...
+                                          crecTD * recTDij * recTDk / recTDBar + ...
+                                          cfum * fumBar;
+        
+        k = k + 1;
+    end
+end
+
+playerData.numQB = data(1).num;
+playerData.numRB = data(2).num;
+playerData.numWR = data(3).num;
+playerData.numTE = data(4).num;
+playerData.numK = data(5).num;
+playerData.numDEF = data(6).num;
+
+playerData.numPlayers = playerData.numQB + playerData.numRB + playerData.numWR + playerData.numTE + playerData.numK + playerData.numDEF;
+
+playerData.performance = zeros(playerData.numPlayers, ns);
+
+
--- trunk/preProcessFiles.m
+++ trunk/preProcessFiles.m
@@ -1,20 +1,52 @@
 function preProcessFiles(week)
 
-fname = 'def_rushing_week_5.data';
-fid = fopen(fname);
-scan = textscan(fid, '%s', 'whitespace', '');
-fclose(fid);
-rawstr = cell2mat(scan{1});
-
-fid = fopen('FullTeamName2Abbrev.data');
+positions = {'matchup'; 'def_rushing'; 'def_passing'; 'def_receiving'; 'qb'; 'rb'; 'wr'; 'te'; 'k'};
+newheaders = {'Sunday, October 14  	Time (EST)  	Tickets  	Network  	Channel  	HD Channel  	Home  	Away  	Westwood One'; ...
+    'Team  	  	Games  	 	Rush  	 	RushYds  	 	RushAvg  	 	RushPG  	 	RushYPG  	 	RushTD  	 	Rush1stD  	 	RushLng'; ...
+    'Team  	  	Games  	 	AttPG  	 	CompPG  	 	Pct  	 	PassYds  	 	PassYPG  	 	PassTD  	 	Int  	 	Pass1stD  	 	Sack  	 	SackYdsL  	 	SackLng'; ...
+    'Team  	  	Games  	 	Rec  	 	RecYds  	 	RecPG  	 	RecYPG  	 	RecTD  	 	Rec1stD  	 	RecLng'; ...
+    'First Last  	Team  	Games  	  	QBRat  	PassComp  	PassAtt  	Pct  	PassYds  	PassYPG  	PassYPA  	PassTD  	Int  	 	Rush  	RushYds  	RushYPG  	RushAvg  	RushTD  	 	Sack  	SackYds  	 	Fum  	FumL'; ...
+    'First Last  	Team  	Games  	  	Rush  	RushYds  	RushYPG  	RushAvg  	RushTD  	 	Rec  	RecYds  	RecYPG  	RecAvg  	Lng  	YAC  	1stD  	RecTD  	 	Fum  	FumL'; ...
+    'First Last  	Team  	Games  	  	Rec  	RecYds  	RecYPG  	RecAvg  	RecLng  	YAC  	Rec1stD  	RecTD  	 	KR  	KRYds  	KRAvg  	KRLong  	KRTD  	 	PR  	PRYds  	PRAvg  	PRLong  	PRTD  	 	Fum  	FumL'; ...
+    'First Last  	Team  	Games  	  	Rec  	RecYds  	RecYPG  	RecAvg  	RecLng  	YAC  	Rec1stD  	RecTD  	 	Rush  	RushYds  	RushYPG  	RushAvg  	RushTD  	 	Fum  	FumL'; ...
+    'First Last  	Team  	Games  	  	FG19  	FG29  	FG39  	FG49  	FG50  	FGM  	FGA  	Pct  	Lng  	 	XPM  	XPA  	Pct  	 	Pts'};
+% Load abbreviation data from a text file located in the data directory
+fid = fopen( fullfile(cd, 'data', 'FullTeamName2Abbrev.data') );
 nameLookup = textscan(fid, '%s %s', 'delimiter', '\t\n', 'multipledelimsasone', 1);
 fclose(fid);
 
-for n = 1:32
-    rawstr = regexprep(rawstr, nameLookup{1,1}(n), nameLookup{1,2}(n));
+% For all the positions...
+for n = 1:length(positions)
+    fname = [positions{n} '_week_' num2str(week) '.data'];
+
+    % Open the raw data file and scan it all into a single string.. easier to
+    % operate on in this form.
+    fin = fopen( fullfile(cd, 'data', ['wk' num2str(week)], 'raw', fname) );
+    fout = fopen( fullfile(cd, 'data', ['wk' num2str(week)], 'processed', fname), 'W');
+    
+    % Write a new file header
+    line = fgetl(fin);
+    fprintf(fout, '%s\n', newheaders{n});
+    
+    while (~feof(fin))
+        % Get a line of text from the input file..
+        line = fgetl(fin);
+       
+        % Then for each team name in the data file, replace it with its
+        % abbreviation.
+        for n = 1:length(nameLookup{1,1})
+            line = regexprep(line, nameLookup{1,1}(n), nameLookup{1,2}(n));
+        end
+    
+        % Also replace anything else that is "nasty" before writing this to a
+        % new file..
+        
+        % The Marion Barber III fix
+        line = regexprep(line, 'Marion(\s*)Barber(\s*)III', 'Marion Barber');
+    
+        % Lastly write the new line to the out file
+        fprintf(fout, '%s\n', line);
+    end
+    fclose(fin);
+    fclose(fout);
 end
-
-
-fid = fopen('def_rushing_week_5p.data','W');
-fprintf(fid, '%s', rawstr);
-fclose(fid);
