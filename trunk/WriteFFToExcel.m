c940a830b7fb1f706cc6b73934bb59c526dd2b87
yamokosk <yamokosk@f0bbfac0-403c-0410-a557-73fa6a0c6ba8>
2007-11-21 16:27:59 +0000

    Write to excel file completed.

--- trunk/WriteFFToExcel.m
+++ trunk/WriteFFToExcel.m
@@ -20,14 +20,36 @@ f = mean(scenarioMatrix, 2);    % Expected projected performance
 
 % Cost of the players
 cost = [pdata.qb.Price; pdata.rb.Price; pdata.wr.Price; pdata.te.Price; pdata.k.Price; pdata.def.Price];
+names = [pdata.qb.Name; pdata.rb.Name; pdata.wr.Name; pdata.te.Name; pdata.k.Name; pdata.def.Name];
 
 % Guess
 xguess = zeros(np, 1);
 
 % Write to excel file
 filename = ['ffdata_wk' num2str(wk) '.xls'];
-xlswrite(filename, {'Vars', 'Cost', 'Coeff', 'Scenarios'});
-xlswrite(filename, [xguess, cost, f, scenarioMatrix], 'A20');
+
+off = 12;
+VarsRange = ['B' num2str(off) ':B' num2str(off+np-1)];
+CostRange = ['C' num2str(off) ':C' num2str(off+np-1)];
+CoeffRange = ['D' num2str(off) ':D' num2str(off+np-1)];
+nqbRange = ['B' num2str(off) ':B' num2str(off+nqb-1)];
+nrbRange = ['B' num2str(off+nqb) ':B' num2str(off+nqb+nrb-1)];
+nwrRange = ['B' num2str(off+nqb+nrb) ':B' num2str(off+nqb+nrb+nwr-1)];
+nteRange = ['B' num2str(off+nqb+nrb+nwr) ':B' num2str(off+nqb+nrb+nwr+nte-1)];
+nkRange = ['B' num2str(off+nqb+nrb+nwr+nte) ':B' num2str(off+nqb+nrb+nwr+nte+nk-1)];
+ndefRange = ['B' num2str(off+nqb+nrb+nwr+nte+nk) ':B' num2str(off+nqb+nrb+nwr+nte+nk+ndef-1)];
+xlswrite(filename, {'VarsRange', off-1, 1, np, 1; 
+                    'CostRange', off-1, 2, np, 1; 
+                    'CoeffRange', off-1, 3, np, 1; 
+                    'QB', off-1, 1, nqb, 1; ...
+                    'RB', nqb+off-1, 1, nrb, 1; ...
+                    'WR', nqb+nrb+off-1, 1, nwr, 1; ...
+                    'TE', nqb+nrb+nwr+off-1, 1, nte, 1; ...
+                    'K',  nqb+nrb+nwr+nte+off-1, 1, nk, 1; ...
+                    'DEF', nqb+nrb+nwr+nte+nk+off-1, 1, ndef, 1}, 'Data', 'C1');
+xlswrite(filename, {'Names', 'Vars', 'Cost', 'Coeff', 'Scenarios'}, 'Data', 'A11');
+xlswrite(filename, names, 'Data', 'A12');
+xlswrite(filename, [xguess, cost, f, scenarioMatrix], 'Data', 'B12');
 
 % Now, in text, specify the ranges of each data group
 
